af7c7048b37c41e5ea3ee8b373dd04ffaae29ff9
Наиль Шаряпов <zolman99@gmail.com>
2020-02-19 22:10:44 +0300

    Updates

---
+++ precache-manifest.2df58d0e913fdbba2ab6423307108c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a973b32bb3bb5c5c9e07f763af363528",
+    "url": "/cvss2-calculator/index.html"
+  },
+  {
+    "revision": "763d44e45a2d8a66be9e",
+    "url": "/cvss2-calculator/static/css/main.b0d1f5b8.chunk.css"
+  },
+  {
+    "revision": "a83c78bb5fef584574b9",
+    "url": "/cvss2-calculator/static/js/2.cb7009ae.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/cvss2-calculator/static/js/2.cb7009ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763d44e45a2d8a66be9e",
+    "url": "/cvss2-calculator/static/js/main.d87ad9e8.chunk.js"
+  },
+  {
+    "revision": "7889b788eb7b401910b3",
+    "url": "/cvss2-calculator/static/js/runtime-main.cd7b01e1.js"
+  }
+]);
